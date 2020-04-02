b0630dead91bf93508e1539459cbbfce8ff92382
gehu <gehu1107@hotmail.com>
2020-04-02 18:14:31 +0800

    [Linear regression]

--- 01-Linear Regression/Linear_Regression.py
+++ 01-Linear Regression/Linear_Regression.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python3
 # encoding: utf-8
-"""
-@author:  liaoxingyu
-@contact: sherlockliao01@gmail.com
+"""linear regression
 """
 
 import matplotlib.pyplot as plt
@@ -10,17 +9,15 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 
+
 x_train = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168],
                     [9.779], [6.182], [7.59], [2.167], [7.042],
                     [10.791], [5.313], [7.997], [3.1]], dtype=np.float32)
-
 y_train = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
                     [3.366], [2.596], [2.53], [1.221], [2.827],
                     [3.465], [1.65], [2.904], [1.3]], dtype=np.float32)
 
-
 x_train = torch.from_numpy(x_train)
-
 y_train = torch.from_numpy(y_train)
 
 
@@ -34,18 +31,19 @@ class linearRegression(nn.Module):
         out = self.linear(x)
         return out
 
-
+#模型
 model = linearRegression()
+
 # 定义loss和优化函数
 criterion = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
+#"""
 # 开始训练
 num_epochs = 1000
 for epoch in range(num_epochs):
     inputs = x_train
     target = y_train
-
     # forward
     out = model(inputs)
     loss = criterion(out, target)
@@ -56,18 +54,31 @@ for epoch in range(num_epochs):
 
     if (epoch+1) % 20 == 0:
         print(f'Epoch[{epoch+1}/{num_epochs}], loss: {loss.item():.6f}')
+#"""
 
+# save/load 模型
+is_save = True
+model_fn = "./linear.pth"
+if is_save:
+    torch.save(model.state_dict(), './linear.pth')
+else:
+    model.load_state_dict(torch.load('./linear.pth'))
+
+#预测模式
 model.eval()
 with torch.no_grad():
     predict = model(x_train)
-predict = predict.data.numpy()
+print(f"{predict.shape}, {y_train.shape}")
+loss = criterion(predict, y_train)
+print(f'loss: {loss.item():.6f}')
 
+
+"""
+predict = predict.data.numpy()
 fig = plt.figure(figsize=(10, 5))
 plt.plot(x_train.numpy(), y_train.numpy(), 'ro', label='Original data')
 plt.plot(x_train.numpy(), predict, label='Fitting Line')
 # 显示图例
 plt.legend() 
 plt.show()
-
-# 保存模型
-torch.save(model.state_dict(), './linear.pth')
+#"""
